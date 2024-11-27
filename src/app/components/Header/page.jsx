32f6d8cda4f34b211d5f4f5c306d69d341f990d6
Raj106292 <rajdas.106292@gmail.com>
2024-11-27 17:52:35 +0600

    menu button & logo added

--- src/app/components/Header/page.jsx
+++ src/app/components/Header/page.jsx
@@ -1,12 +1,21 @@
 import "./style.css";
+import Link from "next/link";
 import Image from "next/image";
+import { IoMdMenu } from "react-icons/io";
+import Button from '@mui/material/Button';
 import logo from "../../../assets/images/logo.png";
 
 const Header = () => {
-    return(
+    return (
         <header>
-            <div className="row">
-                
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-3 d-flex align-items-center part1">
+                        <Button className="button rounded-circle d-flex align-items-center justify-content-center"><IoMdMenu className="text-white" /></Button>
+
+                        <Link href="/" className="mx-2 logo"><Image alt="app_logo" src={logo} /></Link>
+                    </div>
+                </div>
             </div>
         </header>
     )
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,6 @@
-// import localFont from "next/font/local";
-import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header/page";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+import "./globals.css";
 
 export const metadata = {
   title: "MyTube",
