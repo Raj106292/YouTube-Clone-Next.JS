4042453ab629838a7a18fe372c7ec484b167258a
Raj106292 <rajdas.106292@gmail.com>
2024-12-04 15:28:38 +0600

    sidebar menu added

--- src/app/components/Sidebar/page.jsx
+++ src/app/components/Sidebar/page.jsx
@@ -1,8 +1,21 @@
 import { categories } from "@/utils/contents";
+import './style.css';
+import Button from '@mui/material/Button';
 
 const Sidebar = () => {
     return (
-        <></>
+        <div className="sidebar">
+            {
+                categories?.map((category, index) => {
+                    return (
+                        <Button key={index}>
+                            <span className="icon">{category.icon}</span>
+                            {category.name}
+                        </Button>
+                    )
+                })
+            }
+        </div>
     )
 }
 
